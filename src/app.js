6538a37d2474fca78b8fc175ee55a02ada653abc
Shah Mohammad Ansari <shahmohammadt01@gmail.com>
2026-01-31 21:32:05 +0530

    create middleware with dummy auth

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const {adminAuth, userAuth} = require('./middlewares/auth');
 
 const app = express();
 
-app.use('/test', (req, res) => {
-    res.send('this is a test route');
+app.use('/admin', adminAuth);
+
+app.get('/user/login', (req, res) => {
+    res.send("Login user");
 });
 
-app.use('/hello', (req, res) => {
-    res.send('this is hello route');
+app.get('/user/profile', userAuth, (req, res) => {
+    res.send("Show Profile");
 });
+
+app.get('/admin/allData', (req, res) => {
+    res.send("send all data");
+});
+
+app.get('/admin/allDelete', (req, res) => {
+    res.send("deleted");
+});
+
 
 app.listen(3000, () => {
     console.log('App is listening on port 3000.');
